40417de2e699d4b5c051df9a9d30b6e169afafec
danielgomez3 <danielgomezcoder@gmail.com>
2025-05-01 07:14:21 -0400

    update

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "danielgomez3's SQL development flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";  # Nix Options version as well
+  };
+  outputs = inputs@{...}:
+    let 
+      currentSystem = builtins.currentSystem or "x86_64-linux";  # Fallback if not detected
+      supportedSystems = {
+        linux = "x86_64-linux";
+        darwinIntel = "x86_64-darwin"; 
+        darwinAmd = "aarch64-darwin"; 
+        android = "aarch64-linux"; 
+      };
+      pkgs = inputs.nixpkgs.legacyPackages.${currentSystem};
+    in 
+    {
+      devShells.${currentSystem}.default = pkgs.mkShell { 
+        buildInputs = with pkgs; [
+          sqlite sqlint
+ 
+        ];  # deps needed at runtime.
+        GREETING = "Hello, Nix!";
+        shellHook = ''
+          ${pkgs.pfetch}/bin/pfetch
+          echo $GREETING
+        '';
+      };
+
+    };
+}
